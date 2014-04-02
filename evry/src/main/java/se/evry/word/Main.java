fb2b53608dade4f853e42d252a9ae224860a49b8
Tim Mickelson <tim_mickelson@bigfoot.com>
2014-04-02 08:27:04 +0200

    Compile in one single jar with all dependencies.

--- evry/src/main/java/se/evry/word/Main.java
+++ evry/src/main/java/se/evry/word/Main.java
@@ -3,11 +3,40 @@ package se.evry.word;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Main class to launch from command line.
+ * <br><br>
+ * <i>Usage:</i><br>
+ * <code>
+ * $ java -jar evry.jar option option-param
+ * <br><br>
+ * Possible options with option-param are:
+ * <br>
+ * -f file <i>Absolute path to single file with txt or html postfix.</i>
+ * <br>
+ * -d directory <i>Absolute path to folder containing html and/or txt files.</i>
+ * </code>
+ * @author Tim Mickelson
+ *
+ */
 public class Main {
 	Logger logger = LoggerFactory.getLogger(getClass());
 	public static void main(String[] args){
 		Logger logger = LoggerFactory.getLogger(Main.class);
 		logger.info("Kommer jag hit");
-		//System.out.println("KOMMER JAG HIT");
-	}
-}
+		for(String arg : args){
+			logger.info(arg);
+		}
+		
+		//TODO: not always of course
+		usage();
+	}  // end public function main
+	
+	private static void usage(){
+		System.out.println("\nUsage:");
+		System.out.println("$ java -jar evry.jar option option-param");
+		System.out.println("\nPossible option, option-param are:");
+		System.out.println("-f file");
+		System.out.println("-d directory");
+	}  // end public function usage
+} // end class Main
