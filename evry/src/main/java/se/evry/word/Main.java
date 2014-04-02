687261ab31a022520b08fe48f13028659dc0d2bd
Tim Mickelson <tim_mickelson@bigfoot.com>
2014-04-02 08:09:16 +0200

    Main class also in MANIFEST through the pom

---
+++ evry/src/main/java/se/evry/word/Main.java
@@ -0,0 +1,13 @@
+package se.evry.word;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+	Logger logger = LoggerFactory.getLogger(getClass());
+	public static void main(String[] args){
+		Logger logger = LoggerFactory.getLogger(Main.class);
+		logger.info("Kommer jag hit");
+		//System.out.println("KOMMER JAG HIT");
+	}
+}
